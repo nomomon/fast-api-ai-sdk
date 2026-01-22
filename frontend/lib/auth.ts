1a7aa7d5855f1344286f3ef755451d8b9722c421
Mansur Nurmukhambetov <funisinfinity26@gmail.com>
2026-01-22 17:54:40 +0500

    refactor: simplify auth function and remove unused JWT callbacks

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -5,8 +5,6 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 
 interface DecodedToken {
   sub: string;
-  id: string;
-  role: string;
   exp: number;
 }
 
@@ -17,17 +15,13 @@ async function auth(payload: { email: string; password: string }) {
     console.log('Auth Payload:', payload);
 
     const res = await axios.post(url, payload);
-
     const data = await res.data;
-    const accessToken = data.access_token;
 
-    if (res.status === 200 && accessToken) {
-      // We return the token and username to be used in the JWT callback
-      const { id, sub } = jwtDecode<DecodedToken>(accessToken);
+    if (res.status === 200 && data.access_token) {
+      const { sub } = jwtDecode<DecodedToken>(data.access_token);
       return {
-        id,
-        email: sub,
-        accessToken,
+        id: sub,
+        email: payload.email,
       };
     }
     return null;
@@ -53,24 +47,7 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  callbacks: {
-    // TODO: idk what this is doing, gonna fix a bit later
-    async jwt({ token, user }) {
-      // "user" is only available on the first time the JWT is created (on login)
-      if (user) {
-        token.accessToken = user.accessToken;
-        token.email = user.email;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      // Pass the token from the JWT to the client-side session
-      session.user.role = token.role;
-      session.accessToken = token.accessToken;
-      session.user.email = token.email;
-      return session;
-    },
-  },
+  callbacks: {},
 
   pages: {
     signIn: '/login',
